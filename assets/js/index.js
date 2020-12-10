7ad7c3b27930ea27a2769afd61c9ffb97ff7b09c
Léo Delplanque <delplanqueleomail@gmail.com>
2020-12-10 11:57:14 +0100

    Début HTML CSS main page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,42 @@
-//testing the API
+// //testing the API
+// let _div = document.querySelector('.test');
+// async function getSystemElements(){
+//     const response = await fetch('https://api.pandascore.co/rl/teams?token=nj-MFyWKZ9dcHkCJRdFayjnhLzlhlxaVi4wp7T93239-NZRr0gg&per_page=100&page=2');
+//     const body = await response.json();
 
-async function getSystemElements(){
-    const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.pandascore.co/leagues?token=nj-MFyWKZ9dcHkCJRdFayjnhLzlhlxaVi4wp7T93239-NZRr0gg');
-    const body = await response.json();
+//     try{
+//         return body;
+//     }catch{
+//         console.log('error happened', e)
+//     }
 
-    try{
-        return body;
-    }catch{
-        console.log('error happened', e)
-    }
+// }
 
-}
-
-async function create(){
-    const data = await getSystemElements();
+// async function create(){
+//     const data = await getSystemElements();
     
     
-    try{
-        console.log(data)
+//     try{
+//         console.log(data)
+//         data.forEach(el=>{
+//             let divtoAdd= document.createElement('div');
+//             let img = document.createElement('img');
+//             let text = document.createElement('span');
+            
+//             img.setAttribute("src", `${el.image_url}`);
+//             text.innerHTML = el.name;
+//             divtoAdd.appendChild(img);
+//             divtoAdd.appendChild(text)
+            
+//             _div.appendChild(divtoAdd)
+//         });
+       
         
-    }catch{
-        console.log("error")
-    }
+//     }catch{
+//         console.log("error")
+//     }
 
     
-}
+// }
 
-create();
+// create();
