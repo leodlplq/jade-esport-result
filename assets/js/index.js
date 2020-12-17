e23884ee9e7bd95489aa48bf59686177cf9099fc
ad-imac <adamferreira@protonmail.com>
2020-12-17 23:42:23 +0100

    ajout du burger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,12 @@
 //testing the API
 let _div = document.querySelector('.test');
+let hamburger = document.querySelector(('.hamburger'));
+let navLinks = document.querySelector(('.nav_links'));
+let links = document.querySelectorAll(('.nav_links li'));
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+});
