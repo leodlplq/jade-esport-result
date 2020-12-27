cd74882130e00921976a45ac647e2d70947a4d65
ad-imac <adamferreira@protonmail.com>
2020-12-27 19:02:56 +0100

    transformed the burger menu into a cross on click and changed the space between nav_links

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 //testing the API
 let _div = document.querySelector('.test');
 let hamburger = document.querySelector(('.hamburger'));
+let lines = document.querySelectorAll(('.hamburger div'));
 let navLinks = document.querySelector(('.nav_links'));
 let links = document.querySelectorAll(('.nav_links li'));
 
@@ -9,4 +10,7 @@ hamburger.addEventListener("click", () => {
     links.forEach(link => {
         link.classList.toggle("fade");
     });
+    lines[0].classList.toggle("line1_transform");
+    lines[1].classList.toggle("line2_transform");
+    lines[2].classList.toggle("line3_transform");
 });
